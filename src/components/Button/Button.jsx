73340e98ce632b49565a4091d6c7699911bfd89f
Audrey Rasolonjatovo <audrey.rasolonjatovo@servier.com>
2025-03-10 14:26:01 +0100

    feat(button): add Button component with customizable styles and sizes

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,30 @@
+import clsx from "clsx";
+
+const Button = ({ variant = "primary", size = "md", children, ...props }) => {
+  const baseStyles =
+    "font-medium rounded-lg transition duration-200 focus:outline-none hover:cursor-pointer";
+
+  const variantStyles = {
+    primary: "bg-blue-600 text-white hover:bg-blue-700",
+    secondary: "bg-gray-600 text-white hover:bg-gray-700",
+    outline: "border border-gray-600 text-gray-600 hover:bg-gray-100",
+    danger: "bg-red-600 text-white hover:bg-red-700",
+  };
+
+  const sizeStyles = {
+    sm: "px-3 py-1 text-sm",
+    md: "px-4 py-2 text-base",
+    lg: "px-5 py-3 text-lg",
+  };
+
+  return (
+    <button
+      className={clsx(baseStyles, variantStyles[variant], sizeStyles[size])}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
