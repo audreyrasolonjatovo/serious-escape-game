22ccf3b3b571bfc85913d39398a631a0e3d2f504
Audrey Rasolonjatovo <audrey.rasolonjatovo@servier.com>
2025-03-11 20:35:34 +0100

    refactor(tailwind): remove custom color definitions from Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,8 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        customBlue: "#1E3A8A",
-        customBlueLight: "#3B82F6",
-      },
-    },
+      colors: {}
+    }
   },
-  plugins: [],
+  plugins: []
 };
