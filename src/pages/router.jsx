a82b3b3f775f72857706a07c1fb7b2c53a959d7e
Audrey Rasolonjatovo <audrey.rasolonjatovo@servier.com>
2025-03-11 20:14:02 +0100

    feat(router): add RouterComponent for routing with react-router-dom

---
+++ src/pages/router.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "../App";
+
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default RouterComponent;
