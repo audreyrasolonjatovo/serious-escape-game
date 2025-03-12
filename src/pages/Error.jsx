9f56d68dd71636ce58fa51eb7459507be1124741
Audrey Rasolonjatovo <audrey.rasolonjatovo@servier.com>
2025-03-12 14:19:14 +0100

    feat(error): implement custom error page with GIF and navigation options

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
+import { Link, useNavigate } from "react-router-dom";
+
+import Background from "../assets/Background";
+import Button from "../components/Button/Button";
+
+import errorGif from "../assets/error.gif";
+
 export default function Error() {
-  return <div>Error</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="relative flex flex-col items-center justify-center min-h-screen p-4 text-center overflow-hidden">
+      <Background className="absolute inset-0 w-full h-full object-cover" />
+      <div className="relative z-10 bg-white rounded-2xl shadow-lg w-full max-w-md p-6">
+        <span className="text-2xl font-semibold text-gray-900 mb-6 flex items-center justify-center gap-1 flex-col">
+          <h1>OOPS</h1>
+          <img src={errorGif} alt="test" />
+          <p>Cette option n'est pas disponible pour le moment.</p>
+        </span>
+        <div className="flex flex-col gap-2">
+          <Button onClick={() => navigate("/notallowed")}>RESTER</Button>
+          <Link to="/" className="text-customBlue text-sm">
+            Page d'accueil
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
