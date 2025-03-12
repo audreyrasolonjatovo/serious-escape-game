a1b9fa4eef87d8d0f1d325707f3c37fc74b00eb6
Audrey Rasolonjatovo <audrey.rasolonjatovo@servier.com>
2025-03-12 17:06:23 +0100

    feat(error): enhance error page layout and messaging for better user experience

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -13,15 +13,17 @@ export default function Error() {
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen p-4 text-center overflow-hidden">
       <Background className="absolute inset-0 w-full h-full object-cover" />
-      <div className="relative z-10 bg-white rounded-2xl shadow-lg w-full max-w-md p-6">
-        <span className="text-2xl font-semibold text-gray-900 mb-6 flex items-center justify-center gap-1 flex-col">
-          <h1>OOPS</h1>
-          <img src={errorGif} alt="test" />
-          <p>Cette option n'est pas disponible pour le moment.</p>
-        </span>
-        <div className="flex flex-col gap-2">
+      <div className="relative z-10 bg-white rounded-2xl shadow-lg w-full max-w-md p-6 flex flex-col justify-between">
+        <h1 className="text-7xl font-bold pt-5 text-customOrange">OOPS</h1>
+        <img src={errorGif} alt="test" />
+        <p className="text-lg mt-[-40px] text-center font-medium mb-4">
+          Cette page est un peu trop moderne pour notre ami l’homme des
+          cavernes.
+        </p>
+        <p>Essayez encore plus tard...</p>
+        <div className="flex flex-col gap-2 mt-5">
           <Button onClick={() => navigate("/notallowed")}>RESTER</Button>
-          <Link to="/" className="text-customBlue text-sm">
+          <Link to="/" className="text-customBlue text-base">
             Page d'accueil
           </Link>
         </div>
