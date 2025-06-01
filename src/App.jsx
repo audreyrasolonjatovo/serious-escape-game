332d17de5e1881e2a1305b6faadb11212b3b9dde
Audrey Rasolonjatovo <audrey.rasolonjatovo@servier.com>
2025-06-01 22:41:06 +0200

    add bg

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Background from "./assets/Background";
 const App = () => {
   const navigate = useNavigate();
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen p-4 text-center overflow-hidden">
+    <div className="relative flex flex-col items-center justify-center min-h-screen p-4 text-center overflow-hidden bg-[#24226A]">
       <Background className="absolute inset-0 w-full h-full object-cover" />
 
       <div className="relative z-10 bg-white rounded-2xl shadow-lg w-full max-w-md p-6">
