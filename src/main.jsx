c619f80f932aa386cc97f4cdc722ea3242c3c471
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2025-07-09 19:21:56 +0200

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import RouterComponent from "./pages/router.jsx";
-
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(<RouterComponent />);
